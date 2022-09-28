c7e3ab79af1469e41a68e7d84cae7b8c676812e6
Santiago Carmona Gonzalez <dev.santicarmo31@gmail.com>
2022-09-28 09:04:59 -0500

    Initial Commit

---
+++ SearchableTools/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SearchableTools
+//
+//  Created by Andres Restrepo Gonzalez on 28/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchableTools/SearchableToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableToolsApp.swift
+//  SearchableTools
+//
+//  Created by Andres Restrepo Gonzalez on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchableToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
