38ec1ef44500eb1cef606fa7f36ca6852ac42202
Andres Restrepo Gonzalez <andrestrepo95@hotmail.com>
2022-12-23 13:17:27 -0500

    New button "ADD" added to the list, this function let you add Items to the list
    (cherry picked from commit d141da8741c188fbeb580c19000a53b72cd9b647)

--- SearchableTools/ContentView.swift
+++ SearchableTools/ContentView.swift
@@ -10,27 +10,50 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var viewModel = ViewModel()
+    @State var addItemName: String = " "
+    @State var addItem = false
     
     var body: some View {
+        
         NavigationView {
+            
             List {
                 ForEach(viewModel.filteredData, id: \.self) { tool in
                     NavigationLink(destination: Text(tool)) {
                         Text(tool)
-                        }
                     }
-                    
                 }
-                .navigationTitle("SEARCH TOOLS")
-                .foregroundColor(.green)
-                .searchable(text: $viewModel.query, prompt: "Search")
+                
             }
-
+            .navigationTitle("SEARCH TOOLS")
+            .foregroundColor(.blue)
+            .searchable(text: $viewModel.query, prompt: "Search")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { self.addItem.toggle()}, label:{ Text("Add")})
+                }
+            }
+            
+        } .sheet(isPresented: $addItem) {
+            Text("Tool Name: ")
+            TextField("Add an Item", text: self.$addItemName)
+            Button(action: {
+                viewModel.tools.append(self.addItemName)
+                
+                self.addItem.toggle()
+                self.addItemName = ""
+                
+            }, label: {
+                Text("Add")
+            })
+        }
+        }
+        
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    
 }
